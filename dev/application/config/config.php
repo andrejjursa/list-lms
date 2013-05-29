05e8f19f065255633a4f0cd3b3ed00dfbf9b1259
lorddragoo@gmail.com <lorddragoo@gmail.com@94d60886-7cdf-6bf4-00a8-92fe836f7722>
2013-05-29 15:37:47 +0000

    Added some configuration items to application settings form.

--- dev/application/config/config.php
+++ dev/application/config/config.php
@@ -407,7 +407,6 @@ $config['student_login_security_allowed_attempts'] = 5;
 |--------------------------------------------------------------------------
 | Value is in kilobytes, i.e. 1024 = 1 MB.
 */
-
 $config['maximum_solition_filesize'] = 10240;
 
 /**
