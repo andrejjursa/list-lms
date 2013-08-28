e01be75bef7f5ddc3c969061f33865a696e90e6d
lorddragoo <lorddragoo@94d60886-7cdf-6bf4-00a8-92fe836f7722>
2013-08-28 08:56:51 +0000

    Added missing jquery.min.map file. Updated translation selector with ability to filter constants by text writen to particular text box.

--- dev/public/js/translation_selector.js
+++ dev/public/js/translation_selector.js
@@ -6,9 +6,9 @@ var translation_selector = function(jquerySelector) {
             cache: true,
             dataType: 'json',
             success: function(json_data) {
-                var selectorHTML = jQuery('<div class="custom_translations_selector" style="position: absolute; z-index: 50; display: none; border: 1px solid black; background-color: white;"></div>');
+                var selectorHTML = jQuery('<div class="custom_translations_selector" style="position: absolute; z-index: 50; display: none; border: 1px solid black; border-radius: 3px; background-color: white;"></div>');
                 
-                selectorHTML.html(render_content(json_data));
+                selectorHTML.html(render_content(json_data, $(element[0]).val()));
                 $(element[0]).after(selectorHTML);
                 $(element[0]).focus(function() {
                     selectorHTML.css('top', '').css('left', '');
@@ -26,10 +26,10 @@ var translation_selector = function(jquerySelector) {
                     if (canhide) {
                         selectorHTML.hide();
                     }
-                }).change(function() {
+                }).keyup(function() {
+                    selectorHTML.html(render_content(json_data, $(element[0]).val()));
                     selectCurrent(selectorHTML, $(this).val());
-                }).keydown(function() {
-                    selectCurrent(selectorHTML, $(this).val());
+                    applyEvents();
                 });
                 selectorHTML.mouseover(function() {
                     selectorHTML.data('canhide', false);
@@ -39,15 +39,21 @@ var translation_selector = function(jquerySelector) {
                     selectorHTML.data('canhide', true);
                     $(element[0]).focus();
                 });
-                selectorHTML.find('div.selection li').click(function(){
-                    $(element[0]).val('lang:user_custom_' + $(this).attr('rel'));
-                    selectorHTML.find('div.selection li').removeClass('selected');
-                    $(this).addClass('selected');
-                }).css('cursor', 'pointer').css('list-style-type', 'none').mouseover(function() {
-                    $(this).addClass('mouseover');
-                }).mouseout(function() {
-                    $(this).removeClass('mouseover');
-                });
+                
+                var applyEvents = function() {
+                    selectorHTML.find('div.selection li').click(function() {
+                        $(element[0]).val('lang:user_custom_' + $(this).attr('rel'));
+                        selectorHTML.html(render_content(json_data, 'lang:user_custom_' + $(this).attr('rel')));
+                        selectorHTML.find('div.selection li').removeClass('selected');
+                        $(this).addClass('selected');
+                        applyEvents();
+                        $(element[0]).focus();
+                    }).css('cursor', 'pointer').css('list-style-type', 'none').mouseover(function() {
+                        $(this).addClass('mouseover');
+                    }).mouseout(function() {
+                        $(this).removeClass('mouseover');
+                    });
+                };
                 var selectCurrent = function(selectorHTML, value) {
                     selectorHTML.find('div.selection li').each(function() {
                         $(this).removeClass('selected');
@@ -56,16 +62,19 @@ var translation_selector = function(jquerySelector) {
                         }
                     });
                 };
+                
+                applyEvents();
             }
         });
     }
     
-    var render_content = function(data) {
-        var content = '<div class="selection" style="margin: 0; padding: 0; max-height: 6em; overflow-y: auto;">';
+    var render_content = function(data, filter) {
+        var filter_regex = new RegExp(filter);
+        var content = '<div class="selection" style="margin: 0; padding: 0; max-height: 12em; overflow-y: auto;">';
         content += '<ul style="margin: 0; padding: 0;">';
         for (i in data) {
-            if (data[i].text != '') {
-                content += '<li rel="' + (data[i].constant) + '" style="margin: 0; padding: 0; line-height: 1.2em; padding: 0 0.1em;">' + (data[i].text) + '</li>';
+            if (data[i].text != '' && data[i].constant != '' && filter_regex.test('lang:user_custom_' + data[i].constant)) {
+                content += '<li rel="' + (data[i].constant) + '" style="margin: 0; padding: 0; line-height: 1.2em; padding: 0 0.1em;"><span style="width: 50%; display: inline-block;">' + (data[i].text) + '</span> (user_custom_' + (data[i].constant) + ')</li>';
             }
         }
         content += '</ul>';
