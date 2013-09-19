debf98321ba713910e001670943f7d215dca04c2
lorddragoo@gmail.com <lorddragoo@gmail.com@94d60886-7cdf-6bf4-00a8-92fe836f7722>
2013-09-19 17:27:47 +0000

    Added test execution form / script to solution valuation dialog.

--- dev/application/language/sk/tests/java_lang.php
+++ dev/application/language/sk/tests/java_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang['tests_java_type_name'] = 'Java';
-$lang['java_tests_subtype_unit_test_name'] = 'Jednotkový test (Unit test)';
+$lang['java_tests_subtype_unit_test_name'] = 'Unit test';
 $lang['java_tests_config_form_unit_test_zip_file'] = 'ZIP súbor so zdrojovým kódom';
 $lang['java_tests_config_form_unit_test_class_to_run'] = 'Názov súboru s testom';
 $lang['java_tests_config_form_unit_test_class_to_run_hint'] = 'Z bezpečnostných dôvodov súbor s testom, ktorý sa spustí, sa musí volať Test<strong>&lt;váš názov súboru&gt;</strong>.java.<br /><strong>Príklad:</strong> Test<strong>Stvorec</strong>.java';
