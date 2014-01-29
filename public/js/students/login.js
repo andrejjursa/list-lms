19877ca39f01542dd4c213442c2d5dd8bf1e1c78
lorddragoo <lorddragoo@gmail.com>
2014-01-29 00:36:51 +0000

    Added autofocus on e-mail field in login box, made on students request.

--- public/js/students/login.js
+++ public/js/students/login.js
@@ -1,9 +1,13 @@
 jQuery(document).ready(function($) {
-    var loginbox = $('body > #mainwrap > #loginboxwarp > div.internal_padding > #loginbox');
-    if (loginbox.length == 0) {
+    var loginbox = $('body > #mainwrap > #loginboxwrap > div.internal_padding > #loginbox');
+    if (loginbox.length === 0) {
         var window_url = document.URL;
         var url = login_form_url;
         url = url.replace('###URL###', Base64url.encode(window_url));
         window.location = url;
+    }
+    var email_field = $('#id_students_email');
+    if (email_field.length > 0) {
+        email_field.focus();
     }
 });
