069c64d3d69b91be05f1725a342ba034ee8edd6c
lorddragoo@gmail.com <lorddragoo@gmail.com@94d60886-7cdf-6bf4-00a8-92fe836f7722>
2014-02-23 16:59:07 +0000

    Added cache clearing after successful evaluation of task set by tests.

--- dev/application/hooks/clear_cache.php
+++ dev/application/hooks/clear_cache.php
@@ -7,39 +7,15 @@ class Clear_Cache {
         if ($CI->output->get_internal_value(LIST_Output::IV_ACTION_RESULT)) {
             $files = isset($params[$CI->router->directory][$CI->router->class][$CI->router->method]) ? (is_array($params[$CI->router->directory][$CI->router->class][$CI->router->method]) ? $params[$CI->router->directory][$CI->router->class][$CI->router->method] : array($params[$CI->router->directory][$CI->router->class][$CI->router->method])) : array();
             if (count($files)) {
-                //$view_path = APPPATH . 'views/';
                 foreach($files as $file => $group_function) {
                     if ($file == '*') {
                         $CI->parser->clearAllCache();
-                    /*} elseif (substr($file, -2) == '/*') {
-                        $path = substr($file, 0, -1);
-                        if (file_exists($view_path . $path)) {
-                            $this->recursive_clear_cache($path, $view_path, $group_function);
-                        }
-                    } elseif (pathinfo($file, PATHINFO_EXTENSION) == 'tpl' && file_exists($view_path . $file)) {*/
                     } else {
                         $CI->parser->clearCache($file, is_string($group_function) ? $group_function : $group_function($CI));
                     }
                 }
-                //die();
             }
         }
     }
-    
-    /*private function recursive_clear_cache($path, $view_path, $group_function) {
-        $CI =& get_instance();
-        $files_dirs = scandir($view_path . $path);
-        if (count($files_dirs)) {
-            foreach ($files_dirs as $file_dir) {
-                if ($file_dir != '.' && $file_dir != '..') {
-                    if (is_dir($view_path . $path . $file_dir)) {
-                        $this->recursive_clear_cache($path . $file_dir . '/', $view_path, $group_function);
-                    } elseif (pathinfo($view_path . $path . $file_dir, PATHINFO_EXTENSION) == 'tpl') {
-                        $CI->parser->clearCache($path . $file_dir, is_string($group_function) ? $group_function : $group_function($CI));
-                    }
-                }
-            }
-        }
-    }*/
     
 }
