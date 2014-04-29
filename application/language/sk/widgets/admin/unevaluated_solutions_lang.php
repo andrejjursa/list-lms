95ee2176eb7b9c64cfe4d4a1c1055aff8cfbd577
lorddragoo <lorddragoo@gmail.com>
2014-04-29 18:36:25 +0000

    Added widget with list of unevaluated solutions for given course.

---
+++ application/language/sk/widgets/admin/unevaluated_solutions_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['widget_admin_unevaluated_solutions_widget_type_name'] = 'Neohodnotené riešenia';
+$lang['widget_admin_unevaluated_solutions_title'] = 'Neohodnotené riešenia';
+$lang['widget_admin_unevaluated_solutions_configure_form_label_course'] = 'Kurz';
+$lang['widget_admin_unevaluated_solutions_configure_form_field_course'] = 'kurz';
+$lang['widget_admin_unevaluated_solutions_error_course_not_found'] = 'Kurz nie je nastavený, skontrolujte konfiguráciu widgetu.';
+$lang['widget_admin_unevaluated_solutions_list_of_task_sets'] = 'Zoznam zostáv úloh s neohodnotenými riešeniami';
+$lang['widget_admin_unevaluated_solutions_list_of_task_sets_empty'] = 'Momentálne nie je žiadna zostava úloh s neohodnoteným riešením.';
