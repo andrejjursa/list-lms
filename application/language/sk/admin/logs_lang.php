e1c497f8fc3779618cb52da7925dd33be61fb7ea
lorddragoo <lorddragoo@gmail.com>
2014-06-06 19:21:26 +0000

    Added activity log list into administration. Added logging of these activities: - students login into system, - teachers enforced login of student into system, - students attempt to download solution file. Version risen to 1.0.9.

---
+++ application/language/sk/admin/logs_lang.php
@@ -0,0 +1,28 @@
+<?php
+
+$lang['admin_logs_page_title'] = 'Záznamy aktivít';
+
+$lang['admin_logs_log_type_1'] = 'Študentovo nahranie riešenia';
+$lang['admin_logs_log_type_2'] = 'Učiteľovo nahranie riešenia';
+$lang['admin_logs_log_type_3'] = 'Študentovo prihlásenie';
+$lang['admin_logs_log_type_4'] = 'Študentovo stiahnutie riešenia';
+
+$lang['admin_logs_table_header_type'] = 'Typ';
+$lang['admin_logs_table_header_ip_address'] = 'IP adresa';
+$lang['admin_logs_table_header_language'] = 'Jazyk';
+$lang['admin_logs_table_header_message'] = 'Správa';
+$lang['admin_logs_table_header_student'] = 'Študent';
+$lang['admin_logs_table_header_teacher'] = 'Učiteľ';
+$lang['admin_logs_table_header_created'] = 'Vytvorený';
+$lang['admin_logs_table_header_controlls'] = 'Ovládanie';
+$lang['admin_logs_table_header_for_student'] = 'Pre študenta';
+$lang['admin_logs_table_header_task_set'] = 'Zostava úloh / projekt';
+$lang['admin_logs_table_header_course'] = 'Kurz';
+$lang['admin_logs_table_header_filename'] = 'Meno súboru';
+$lang['admin_logs_table_content_no_logs'] = 'Momentálne tu nie sú žiadne záznamy.';
+$lang['admin_logs_table_button_details'] = 'Detaily';
+
+$lang['admin_logs_filter_field_label_type'] = 'Filtrovať podľa typu';
+$lang['admin_logs_filter_submit_button'] = 'Použiť';
+
+$lang['admin_logs_log_not_found'] = 'Záznam sa nenašiel.';
--- application/models/log.php
+++ application/models/log.php
@@ -9,6 +9,8 @@ class Log extends DataMapper {
     
     const LOG_TYPE_STUDENT_SOLUTION_UPLOAD = 1;
     const LOG_TYPE_TEACHER_SOLUTION_UPLOAD = 2;
+    const LOG_TYPE_STUDENT_LOGIN = 3;
+    const LOG_TYPE_STUDENT_SOLUTION_DOWNLOAD = 4;
 
     public $has_one = array(
         'student',
@@ -75,6 +77,68 @@ class Log extends DataMapper {
         $this->additional_data = serialize(array());
         $this->affected_table = 'solutions';
         $this->affected_row_primary_id = (int)$table_id;
+        return $this->save();
+    }
+    
+    /**
+     * Add log message about student login into system.
+     * @param string $message message text.
+     * @param int|Teacher $teacher teacher id or teacher model.
+     * @param int|Student $student student id or student model.
+     * @param string $language language idiom, NULL means autodetect.
+     * @return boolean save operation status.
+     */
+    public function add_student_login_log($message, $teacher, $student, $language = NULL) {
+        if (is_null($language)) { $language = $this->lang->get_current_idiom(); }
+        $this->language = $language;
+        $this->message = $message;
+        $this->ip_address = $_SERVER['REMOTE_ADDR'];
+        if (is_numeric($student)) {
+            $this->student_id = (int)$student;
+        } elseif ($student instanceOf Student) {
+            $this->student_id = $student->id;
+        } else {
+            $this->student_id = NULL;
+        }
+        if (is_numeric($teacher)) {
+            $this->teacher_id = (int)$teacher;
+        } elseif ($teacher instanceOf Teacher) {
+            $this->teacher_id = $teacher->id;
+        } else {
+            $this->teacher_id = NULL;
+        }
+        $this->log_type = self::LOG_TYPE_STUDENT_LOGIN;
+        $this->additional_data = serialize(array());
+        return $this->save();
+    }
+    
+    /**
+     * Add log message about solution file download from student interface.
+     * @param string $message message text.
+     * @param int|Student $student student id or student model.
+     * @param string $solution_file name of solution file.
+     * @param int $task_set_id id of task set from which is file downloaded.
+     * @param string $language language idiom, NULL means autodetect.
+     * @return boolean save operation status.
+     */
+    public function add_student_solution_download_log($message, $student, $solution_file, $task_set_id, $language = NULL) {
+        if (is_null($language)) { $language = $this->lang->get_current_idiom(); }
+        $this->language = $language;
+        $this->message = $message;
+        $this->ip_address = $_SERVER['REMOTE_ADDR'];
+        if (is_numeric($student)) {
+            $this->student_id = (int)$student;
+        } elseif ($student instanceOf Student) {
+            $this->student_id = $student->id;
+        } else {
+            $this->student_id = NULL;
+        }
+        $this->teacher_id = NULL;
+        $this->log_type = self::LOG_TYPE_STUDENT_SOLUTION_DOWNLOAD;
+        $this->additional_data = serialize(array(
+            'task_set_id' => $task_set_id,
+            'solution_file' => $solution_file,
+        ));
         return $this->save();
     }
     
