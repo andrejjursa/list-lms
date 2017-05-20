0f6a78d553125ed4619a6ed19982bdbb60e157cd
Andrej Jursa <dragoo@thelambda.sk>
2017-05-20 22:16:35 +0200

    Changed histogram generation script + added mean value and standard deviation ranges.

--- public/js/admin_solutions/histogram.js
+++ public/js/admin_solutions/histogram.js
@@ -16,20 +16,23 @@ var refresh_points_overview = function() {
         data: data,
         success: function(data) {
             $(targetID).html('');
-            var chartsdata = create_histogram(data);
+            var step = 0.5;
+            var statistics = compute_statistics(data, step);
+            var additionalData = prepare_graph_statistical_data(statistics);
 
             Highcharts.chart('valuationCharts', {
-                chart: {
-                    type: 'column'
-                },
                 title: {
                     text: chartmessages.chartTitle
                 },
+                plotOptions: {
+                    marker: { enabled: false }
+                },
                 xAxis: {
-                    categories: chartsdata.labels,
                     title: {
                         text: chartmessages.xAxis
-                    }
+                    },
+                    plotLines: additionalData.plotLines,
+                    plotBands: additionalData.plotBands
                 },
                 yAxis: {
                     title: {
@@ -39,7 +42,10 @@ var refresh_points_overview = function() {
                 series: [
                     {
                         name: chartmessages.yAxis,
-                        data: chartsdata.y
+                        type: 'column',
+                        data: histogram(data, step),
+                        pointPlacement: 'between',
+                        pointRange: step
                     }
                 ]
             });
@@ -47,62 +53,115 @@ var refresh_points_overview = function() {
     });
 };
 
-var create_histogram = function(data) {
-    var histogram = {
-        'x': [],
-        'labels': [],
-        'y': []
-    };
-    var dataXMin = Infinity;
-    var dataXMax = -Infinity;
-    var haveData = false;
-    for (var x in data) {
-        haveData = true;
-        var nx = parseFloat(x);
-        dataXMax = Math.ceil(Math.max(dataXMax, nx));
-        dataXMin = Math.floor(Math.min(dataXMin, nx));
-    }
-    if (!haveData) {
-        return histogram;
+function histogram(data, step) {
+    var histo = {},
+        arr = [];
+
+    // Group down
+    for (var i in data) {
+        var x = Math.floor(parseFloat(i) / step) * step;
+        if (!histo[x]) {
+            histo[x] = 0;
+        }
+        histo[x]+=data[i];
     }
 
-    var diff = dataXMax - dataXMin;
-    var bars = Math.max(Math.ceil(diff) * 2, 1);
-    var barsWidth = (diff / bars).toFixed(3);
-
-    var barsObject = {};
-
-    for (var b = 0; b < bars; b++) {
-        var bmin = b * barsWidth + dataXMin;
-        var bmax = (b + 1) * barsWidth + dataXMin;
-        if (b + 1 == bars) { bmax = dataXMax; }
-        var bavg = (bmin + bmax) / 2;
-        barsObject[bavg] = {
-            'bmin': bmin.toFixed(3),
-            'bmax': bmax.toFixed(3),
-            'count': 0,
-        };
-        for (var x in data) {
-            var nx = parseFloat(x);
-            if (nx >= bmin && ((b == bars - 1 && nx <= bmax) || (b < bars - 1 && nx < bmax))) {
-                barsObject[bavg].count += parseInt(data[x]);
-            }
+    // Make the histo group into an array
+    for (var x in histo) {
+        if (histo.hasOwnProperty((x))) {
+            arr.push([parseFloat(x), histo[x]]);
         }
     }
 
-    var xs = [];
-    var labels = [];
-    var ys = [];
+    // Finally, sort the array
+    arr.sort(function (a, b) {
+        return a[0] - b[0];
+    });
 
-    for (var bavg in barsObject) {
-        xs.push(bavg);
-        labels.push(barsObject[bavg].bmin + ' ' + chartmessages.to + ' ' + barsObject[bavg].bmax);
-        ys.push(barsObject[bavg].count);
+    return arr;
+}
+
+var compute_statistics = function (data, step) {
+    var output = {
+        mean: 0,
+        sd: 0,
+        min: Infinity,
+        max: -Infinity
+    };
+
+    if (data.length == 0) {
+        return output;
     }
 
-    histogram.x = xs;
-    histogram.labels = labels;
-    histogram.y = ys;
+    var sum = 0, count = 0;
+    for (var x in data) {
+        var nx = parseFloat(x);
+        count += parseInt(data[x]);
+        sum += nx * parseInt(data[x]);
+        output.min = Math.min(output.min, nx);
+        output.max = Math.max(output.max, nx);
+    }
+    output.mean = sum / count;
+    output.min = Math.floor(output.min / step) * step;
+    output.max = Math.floor(output.max / step) * step + step;
 
-    return histogram;
+    var variances = 0;
+    for (var x in data) {
+        var nx = parseFloat(x);
+        for (var i = 0; i < parseInt(data[x]); i++) {
+            variances += (nx - output.mean) * (nx - output.mean);
+        }
+    }
+    output.sd = Math.sqrt(variances / count);
+
+    return output;
+};
+
+var prepare_graph_statistical_data = function(statistics) {
+    var output = {
+        plotLines: [],
+        plotBands: []
+    };
+
+    if (statistics.min > statistics.max) {
+        return output;
+    }
+
+    output.plotLines = [
+        {value: statistics.mean.toFixed(3), width: 2, color: '#666', zIndex: 10, dashStyle: 'Dash', label: {
+            text: 'm', rotation: 0, align: 'center', x: 0, y: -5, style: {fontSize: '10px'}
+        }}
+    ];
+
+    for (var i = -3; i <= 3; i++) {
+        if (i != 0) {
+            var s = parseFloat((statistics.sd * i).toFixed(3)) + parseFloat(statistics.mean.toFixed(3));
+                var plotLine = {
+                    value: s.toFixed(3), width: 1, color: '#999', zIndex: 10, dashStyle: 'Dash', label: {
+                        text: i + 's', rotation: 0, align: 'center', x: 0, y: -5, style: {fontSize: '10px'}
+                    }
+                };
+                output.plotLines.push(plotLine);
+        }
+    }
+
+    for (var i = 1; i <= 3; i++) {
+        var sp = parseFloat((statistics.sd * i).toFixed(3)) + parseFloat(statistics.mean.toFixed(3));
+        var sm = parseFloat((statistics.sd * (-i)).toFixed(3)) + parseFloat(statistics.mean.toFixed(3));
+        var plotBand = {
+            from: sm.toFixed(3), to: sp.toFixed(3), color: 'rgba(184,210,236,.2)', zIndex: 0
+        };
+        output.plotBands.push(plotBand);
+    }
+
+    return output;
+};
+
+var normal_distribution = function(x, mean, sd) {
+    var denominator1 = Math.sqrt(2 * Math.PI * sd * sd);
+    var denominator2 = 2 * sd * sd;
+    var nominator = - (x - mean) * (x - mean);
+    var exponent = nominator / denominator2;
+    var e = Math.pow(Math.E, exponent);
+    return e / denominator1;
 };
