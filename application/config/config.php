614ca65f2c4a8bda07856f383bf1e281d270644f
Andrej Jursa <dragoo@thelambda.sk>
2021-09-19 22:04:09 +0200

    Some basic docker development environment.

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = getenv('LIST_CONFIG_BASE_URL');
 
 /*
 |--------------------------------------------------------------------------
